cac39471815e7003ed98b8f16b7d59a0f9f926cb
bearsprogrammer <jhkim@incorl.hanyang.ac.kr>
2019-10-16 15:10:29 +0900

    init commit

---
+++ script/create_node.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+workspace=ROS2CoNA
+
+cd 
+echo -e "Please input node(package) name: \c "
+read node 
+tput setaf 1
+echo "======================="
+echo "$node node location is ~/${workspace}/src/$node/"
+echo "======================="
+tput sgr0
+cd ~/${workspace}/src/
+catkin_create_pkg $node std_msgs roscpp
+echo "source ~/${workspace}/devel/setup.bash" >> ~/.bashrc
+
+cd ~/${workspace}/src/$node/src/
+cp ~/${workspace}/templete/templete.cpp ./
+cp ~/${workspace}/templete/CMakeLists.txt ../ 
+cp ~/${workspace}/package.xml ../ 
+mv templete.cpp $node.cpp
+cd ~/${workspace} 
+
+tput setaf 2
+echo "======================="
+echo "$node package is created. Please change few word in CMakeLists.txt and package.xml.."
+echo "======================="
+tput sgr0
+
+source ~/.bashrc
