8b5db7271d0f45a7b8073dcb504f305799bd822f
Heeseok <heidenletter@gmail.com>
2019-10-16 16:49:03 +0900

    add test.cpp

---
+++ src/test/src/test.cpp
@@ -0,0 +1,13 @@
+//ROS
+#include <ros/ros.h>
+
+int main(int argc, char** argv)
+{
+	ros::init(argc, argv, "templete");
+	ROS_INFO("%s", ros::this_node::getName().c_str());
+
+	ros::NodeHandle nh;
+	
+	ros::spin();
+	return 0;
+}
